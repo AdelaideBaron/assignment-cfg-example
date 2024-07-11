34e679fa4556f234780561fb47a944aa374bd545
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-11 17:22:43 +0100

    getting information from app.yaml

--- src/main/java/com/example/assignment_cfg_example/EndpointController.java
+++ src/main/java/com/example/assignment_cfg_example/EndpointController.java
@@ -1,6 +1,7 @@
 package com.example.assignment_cfg_example;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,28 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class EndpointController {
 
+    @Autowired
+    private Config config;
+
     @GetMapping("/getPenguin")
-    public static ResponseEntity<String> getPenguin(@RequestParam String name, @RequestHeader(value = "membership-id") String membershipId){
+    public ResponseEntity<String> getPenguin(@RequestParam String name, @RequestHeader(value = "membership-id") String membershipId){
         log.info("/getPenguin accessed");
         log.debug("name supplied: " + name);
         log.info("Membership calling: " + membershipId);
 
         Penguin penguin = new Penguin(name, "Black & White");
+        String message = config.getMessage();
 
-
-        return ResponseEntity.ok(penguin.toString());
+        return ResponseEntity.ok(penguin.toString() + " MESSAGE: " + message);
     }
 //    http://localhost:8080/getPenguin?name=percy + headers
-
-//    Penguin(name=percy, colour=Black & White)
-    // key: value
-
-    // might want a response more like the following
-//{
-//    "name": "penguin",
-//    "colour": xyz
-//}
-
-
 
 }
