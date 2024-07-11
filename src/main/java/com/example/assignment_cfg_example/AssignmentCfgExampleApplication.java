5f8f690b07a4dab2448b0925ef0bd798958f4b28
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-11 16:15:22 +0100

    add warning log to startup

--- src/main/java/com/example/assignment_cfg_example/AssignmentCfgExampleApplication.java
+++ src/main/java/com/example/assignment_cfg_example/AssignmentCfgExampleApplication.java
@@ -1,13 +1,16 @@
 package com.example.assignment_cfg_example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AssignmentCfgExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentCfgExampleApplication.class, args);
+		log.warn("starting application");
 	}
 
 }
