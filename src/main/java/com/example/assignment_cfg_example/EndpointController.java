e2eb5ad88004649618fda412ff5998ef86d35fe0
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-11 16:21:21 +0100

    using request param

--- src/main/java/com/example/assignment_cfg_example/EndpointController.java
+++ src/main/java/com/example/assignment_cfg_example/EndpointController.java
@@ -3,6 +3,7 @@ package com.example.assignment_cfg_example;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class EndpointController {
 
     @GetMapping("/getPenguin")
-    public static ResponseEntity<String> getPenguin(){
+    public static ResponseEntity<String> getPenguin(@RequestParam String name){
         log.info("/getPenguin accessed");
-        return ResponseEntity.ok("penguin");
+        log.debug("name supplied: " + name);
+        return ResponseEntity.ok("penguin called: " + name);
     }
+//    http://localhost:8080/getPenguin?name=percy
+
+
+
 }
