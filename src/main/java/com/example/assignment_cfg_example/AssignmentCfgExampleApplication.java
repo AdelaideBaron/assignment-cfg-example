5fed4b467355506a78fcaa5adf594ef057cc001b
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-11 16:12:33 +0100

    initial commit

---
+++ src/main/java/com/example/assignment_cfg_example/AssignmentCfgExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment_cfg_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentCfgExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentCfgExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/assignment_cfg_example/AssignmentCfgExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment_cfg_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentCfgExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
