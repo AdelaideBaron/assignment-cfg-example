e67539b4dd9d0d97f7fb60e858787c0b3a90a1a4
AdelaideBaron <baronadelaide@hotmail.com>
2024-07-11 17:08:29 +0100

    adding param and header to /getPenguin

--- src/main/java/com/example/assignment_cfg_example/EndpointController.java
+++ src/main/java/com/example/assignment_cfg_example/EndpointController.java
@@ -3,6 +3,7 @@ package com.example.assignment_cfg_example;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class EndpointController {
 
     @GetMapping("/getPenguin")
-    public static ResponseEntity<String> getPenguin(@RequestParam String name){
+    public static ResponseEntity<String> getPenguin(@RequestParam String name, @RequestHeader(value = "membership-id") String membershipId){
         log.info("/getPenguin accessed");
         log.debug("name supplied: " + name);
+        log.info("Membership calling: " + membershipId);
         return ResponseEntity.ok("penguin called: " + name);
     }
-//    http://localhost:8080/getPenguin?name=percy
+//    http://localhost:8080/getPenguin?name=percy + headers
 
 
 
